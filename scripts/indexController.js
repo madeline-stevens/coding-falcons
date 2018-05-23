3b0c6cfa0c2b1d88547dada78bef476aa4afa29a
Madeline Stevens <stevensm1987@gmail.com>
2018-05-23 14:51:55 -0700

    home, about and scholarship are working

--- scripts/indexController.js
+++ scripts/indexController.js
@@ -1,15 +1,15 @@
-// 'use strict';
+'use strict';
 
-// var app = app || {};
+var app = app || {};
 
-// (function(module) {
-//   const indexController = {};
+(function(module) {
+  const indexController = {};
 
-//   indexController.index = () => {
-//     $('#about').hide();
-//     $('#home').show();
-//     $('#scholarships').hide();
-//     $('#school-program').hide();
-//   };
-//   module.indexController = indexController;
-// })(app);
+  indexController.index = () => {
+    $('#about').hide();
+    $('#home').show();
+    $('#scholarships').hide();
+    $('#school-program').hide();
+  };
+  module.indexController = indexController;
+})(app);
--- scripts/routes.js
+++ scripts/routes.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var app = app || {};
-// page('/home', app.indexController.index);
+// page('/home', app.indexController.index); don't need right now for just clicking on home
 page('/about', app.aboutController.index);
-// page('/scholarships', app.scholarshipController.index);
+page('/scholarships', app.scholarshipController.index);
 
 page();
