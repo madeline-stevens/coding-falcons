8f069f635aa1f7a70c8772c3aa16a3333bba7944
Madeline Stevens <stevensm1987@gmail.com>
2018-06-02 22:17:31 -0700

    no more errors in chrome, got rid of public in scripts at bottom of index, now routing is working

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const client =  new pg.Client(conString);
 
 app.listen(PORT, () => console.log(`Listening on port 3000`));
 client.on('error', err => console.error(err));
-app.use(cors())
+app.use(cors());
 app.use(express.static('./public'));
 
 
