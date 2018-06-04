0903033e631b4b94b1023d083973a3c402c8913a
Madeline Stevens <stevensm1987@gmail.com>
2018-06-03 22:52:36 -0700

    hadn't saved routes.js. also is server.js even doing anything

--- public/scripts/routes.js
+++ public/scripts/routes.js
@@ -11,12 +11,11 @@ $(document).ready(function() {
 });
 
 var app = app || {};
-// page('/home', app.indexController.index); don't need right now for just clicking on home
+// page('/', app.indexController.index); don't need right now for just clicking on home
 page('/about', app.aboutController.index);
 page('/scholarships', app.scholarshipController.index);
 page('/programs', app.programsController.index);
 page('/contact', app.contactController.index);
-// page('/', app.indexController.index);
 
 
 page();
