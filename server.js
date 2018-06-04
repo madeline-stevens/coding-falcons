3329bb94d4e129659335b9ae9343a61db5de7cd4
Madeline Stevens <stevensm1987@gmail.com>
2018-06-03 20:24:21 -0700

    added more to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 //setting up our npm stuff and creating app and client
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const pg = require('pg');
 const cors = require('cors');
 const PORT= process.env.PORT || 3000;
@@ -10,9 +11,11 @@ const conString = process.env.DATABASE_URL;
 const client =  new pg.Client(conString);
 
 client.connect();
+client.on('error', err => console.error(err));
 app.listen(PORT, () => console.log(`Listening on port 3000`));
 // client.on('error', err => console.error(err));
 app.use(cors());
+app.use(bodyParser.json());
 app.use(express.static('./public'));
 
 
