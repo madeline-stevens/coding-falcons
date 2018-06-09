461ec61febf86c485952cbc34668d3db88133515
Madeline Stevens <stevensm1987@gmail.com>
2018-06-09 13:50:52 -0700

    added contact get route in server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.static('./public'));
 //in my example there are just the following section IDs in index but the only example is for querying a sql database.... 
 
 app.get('/about', (req, res) => res.sendFile('index.html', {root: './public'}));
-// app.get('/contact', (req, res) => res.sendFile('index.html', {root: './public'}));
+app.get('/contact', (req, res) => res.sendFile('index.html', {root: './public'}));
 // app.get('/scholarships', (req, res) => res.sendFile('index.html', {root: './public'}));
 // app.get('/programs', (req, res) => res.sendFile('index.html', {root: './public'}));
 
