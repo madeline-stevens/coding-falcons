ecdde354e8ebcb2edb09d6ec5b16897ab2edd9e4
Madeline Stevens <stevensm1987@gmail.com>
2018-06-13 10:32:20 -0700

    refresh not working on routes

--- public/scripts/routes.js
+++ public/scripts/routes.js
@@ -1,12 +1,12 @@
 'use strict';
 
-$(document).ready(function() {
-  $('#about').hide();
-  $('#scholarships').hide();
-  $('#programs').hide();
-  $('#contact').hide();
-  $('#home').show();
-});
+// $(document).ready(function() {
+//   $('#about').hide();
+//   $('#scholarships').hide();
+//   $('#programs').hide();
+//   $('#contact').hide();
+//   $('#home').show();
+// });
 
 
 var app = app || {};
@@ -15,6 +15,5 @@ page('/about', app.aboutController.about);
 page('/scholarships', app.scholarshipController.scholarships);
 page('/programs', app.programsController.programs);
 page('/contact', app.contactController.contact);
-
 
 page();
