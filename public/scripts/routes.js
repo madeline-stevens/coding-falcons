0a5f6aded397c03bf85497310a0631e83108b5c5
Madeline Stevens <stevensm1987@gmail.com>
2018-06-14 18:59:38 -0700

    tab content is hiding on page load but refreshing will always take you back to home

--- public/scripts/routes.js
+++ public/scripts/routes.js
@@ -1,5 +1,13 @@
 'use strict';
 
+$(document).ready(function() {
+  $('#about').hide();
+  $('#scholarships').hide();
+  $('#programs').hide();
+  $('#contact').hide();
+  $('#home').show();
+});
+
 
 
 var app = app || {};
@@ -8,7 +16,6 @@ page('/about', app.aboutController.about);
 page('/scholarships', app.scholarshipController.scholarships);
 page('/programs', app.programsController.programs);
 page('/contact', app.contactController.contact);
-// page('/home', app.homeController.home);
 
 
 page();
